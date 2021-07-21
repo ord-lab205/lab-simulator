6311b45318ab587ffc0d90235ff3d06581f18a1b
bya2 <byaa1972@gmail.com>
2021-07-21 13:45:45 +0900

    Serves static assets

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.SERVER_PORT
-const app = experss();
+const app = express();
+
+// Servers static assets
+app.use(express.static(path.join(__dirname + '/Railway-Track-Monitoring-master')));
+app.use(express.urlencoded({ extended: true }));
 
 const server = app.listen(PORT, () => console.log('ok.'));
