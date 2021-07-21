e48606d5b0efeea3b03aff048e43cb88e64145d6
bya2 <byaa1972@gmail.com>
2021-07-21 23:04:57 +0900

    Execute query & Read data

--- db.js
+++ db.js
@@ -8,4 +8,24 @@ oracledb.getConnection(dbconfig, (err, conn) => {
     return;
   }
   console.log('Connected oracleDB.');
+
+  const sql = 'select * from anyT';
+  const bindParams = {};
+  const options = { outFormat: oracledb.OBJECT };
+
+  conn.execute(sql, bindParams, options, (err, result) => {
+    if (err) throw err;
+    console.log('Query read success.');
+    
+    console.log('result: ', result);
+
+    dataStr = JSON.stringify(result);
+    console.log('dataStr: ', dataStr);
+
+    arrStr = JSON.stringify(result.rows);
+    console.log('arrStr: ', arrStr);
+
+    const arr = JSON.parse(arrStr);
+    console.log('arr:', arr);
+  })
 });
--- query.sql
+++ query.sql
@@ -1,3 +1,4 @@
+/*
 -- 계정 앞에 'C##'을 붙이지 않고 생성하기 위한 세션 설정
 alter session set "_ORACLE_SCRIPT"=true;
 
@@ -12,3 +13,14 @@ grant connect, resource to lee;
 
 -- 데이터베이스 이름 확인
 select name from v$database;
+*/
+
+-- 테이블 생성
+create table anyT (
+  id int primary key,
+  name varchar2(20) not null
+);
+
+-- 튜플 삽입
+insert into anyT values(1, '이진형');
+insert into anyT values(2, 'Jinhyeong');
