d771889b349f1c33e7768d6ee82f3b831c39c163
bya2 <byaa1972@gmail.com>
2021-07-22 17:59:50 +0900

    Control arudino using serialport & fix: Rename database config

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const oracledb = require('oracledb');
 
-const dbconfig = require('./dbconfig.json');
+const dbconfig = require('./dbConfig');
 
 const run = async () => {
   let conn;
@@ -10,25 +10,21 @@ const run = async () => {
 
     console.log('***Connected to OracleDB:');
 
-    const sql = 'select * from anyT';
-    const bindParams = {};
-    const options = { outFormat: oracledb.OBJECT };
-
-    conn.execute(sql, bindParams, options, (err, result) => {
-      if (err) throw err;
-      console.log('Query read success.');
+    const selectTable = () => {
+      const sql = 'select * from anyT';
+      const bindParams = {};
+      const options = { outFormat: oracledb.OBJECT };
       
-      console.log('result: ', result);
-  
-      dataStr = JSON.stringify(result);
-      console.log('dataStr: ', dataStr);
-  
-      arrStr = JSON.stringify(result.rows);
-      console.log('arrStr: ', arrStr);
-  
-      const arr = JSON.parse(arrStr);
-      console.log('arr:', arr);
-    })
+      conn.execute(sql, bindParams, options, (err, result) => {
+        const arrStr = JSON.stringify(result.rows);
+        console.log(arrStr);
+        const arr = JSON.parse(arrStr); // (=result.rows)
+        console.log(arr);
+      });
+    }
+
+    selectTable();
+
   } catch(err) {
     console.log('***Error in processing:\n', err);
   } finally {
@@ -43,3 +39,43 @@ const run = async () => {
 }
 
 run();
+
+//----- Serialprot -----//
+
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+
+const port = new SerialPort('/dev/tty-usbserial1', {
+  baudRate: 57900
+});
+
+const parser = new Readline();
+
+port.write('Main screen turn on', err => {
+  if (err) {
+    return console.log('Error on write: \n', err.message);
+  }
+  console.log('Message written.');
+});
+
+port.on('error', err => {
+  if (err) {
+    return console.log('Error on write: ', err.message);
+  }
+  console.log('Message written.');
+})
+
+port.on('open', () => {
+  // open logic.
+})
+
+port.on('readable', () => {
+  console.log('Data', port.read);
+})
+
+port.on('data', data => {
+  console.log('Data', data);
+})
+
+const lineStream = port.pipe(new Readline());
+
