934834939ae075dab1e557edbc34f59361b34e8a
bya2 <byaa1972@gmail.com>
2021-08-11 12:57:55 +0900

    write: InsertRun의 query, options 변경

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const oracledb = require('oracledb');
-const config = require('./dbconfig');
+const config = require('./config/oracle');
 const demoSetup = require('./demosetup');
 const { intergrationSensors: stmts } = require('./statements');
 
+// 데이터 베이스 초기화
 const init = async () => {
   let conn;
   try {
@@ -27,12 +28,14 @@ const init = async () => {
   }
 }
 
+
 const selectRun = async aTable => {
   let conn;
 
   try {
     conn = await oracledb.getConnection(config);
 
+    // 특정 테이블을 ROW의 순서대로 역정렬하고 첫번째 ROW를 SELECT
     const sql = `SELECT * FROM (SELECT * FROM ${aTable} ORDER BY ROWNUM DESC) WHERE ROWNUM = 1`,
           bindParams = {},
           options = { outFormat: oracledb.OUT_FORMAT_OBJECT }; // oracledb.OBJECT are deprecated but still usable.
@@ -61,6 +64,7 @@ const intergrationRun = async () => {
   try {
     conn = await oracledb.getConnection(config);
 
+    // 모든 센서 테이블을 통합한 tmp_all_sensor 테이블 생성
     for (const s of stmts) {
       try {
         await conn.execute(s);
@@ -70,14 +74,20 @@ const intergrationRun = async () => {
     }
     await conn.commit();
 
+    // tmp_all_sensor 테이블 SELECT
     const sql = 'SELECT * FROM tmp_all_sensor',
           bindParams = {},
-          options = {outFormat: oracledb.OUT_FORMAT_OBJECT };
+          options = { outFormat: oracledb.OUT_FORMAT_OBJECT };
 
     const result = await conn.execute(sql, bindParams, options);
     const row = result.rows[0];
 
-    return Object.keys(row).filter(s => row[s] > 80);
+    // 수치가 기준을 초과하는 필드 이름 추출(위험 요소)
+    const factors = Object.keys(row).filter(s => row[s] > 80);
+
+    // RISK 필드와 DETAIL 필드 내용 추가
+    row
+    return row;
 
   } catch (err) {
     console.error('*Error in processing.\n', err.message);
@@ -99,14 +109,14 @@ const insertRun = async (aTable, value) => {
   try {
     conn = await oracledb.getConnection(config);
 
-    const sql = 'INSERT INTO ' + aTable + ' VALUES (anyT_seq.NEXTVAL, :NAME)',
+    const sql = `INSERT INTO ${aTable} VALUES (${aTable}_seq.NEXTVAL, :DATA, SYSDATE)`,
           bindParams = [value],
           options = {
             autoCommit: true,
             bindDefs: {
-              NAME: { type: oracledb.DB_TYPE_VARCHAR }
+              DATA: { type: oracledb.DB_TYPE_NUMBER }
             },
-            outFormat: oracledb.OUT_FORMAT_OBJECT
+            // outFormat: oracledb.OUT_FORMAT_OBJECT
           };
 
     await conn.execute(sql, bindParams, options);
