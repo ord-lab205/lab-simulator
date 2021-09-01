382f3ce00c0383a6643fc1e1f338013707c5ef9b
bya2 <byaa1972@gmail.com>
2021-09-01 17:10:51 +0900

    폴더 이동

---
+++ src/config/oracle.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports = {
+  user          : process.env.NODE_ORACLEDB_USER,
+  password      : process.env.NODE_ORACLEDB_PASSWORD,
+  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING,
+}
