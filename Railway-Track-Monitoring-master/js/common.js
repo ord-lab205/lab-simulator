e94ce35d72de9f4c69faa6a05816266a24105fee
bya2 <byaa1972@gmail.com>
2021-09-30 23:42:33 +0900

    [정리] 삭제된 파일에 대한 commit

--- Railway-Track-Monitoring-master/js/common.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  var windowHeight =$('body').height();
-  var contentHeight = windowHeight - 96;
-  $('.right').css({
-    'min-height': contentHeight+'px'
-  })
-})
--- Railway-Track-Monitoring-master/remark.mk
+++
@@ -1,4 +0,0 @@
-时间插件文档地址： https://demo.demohuo.top/jquery/39/3955/demo/
-引用文件： https://demo.demohuo.top/jquery/39/3955/demo/js/plugins/moment.min.js
-https://demo.demohuo.top/jquery/39/3955/demo/js/datepicker.all.js
-https://demo.demohuo.top/jquery/39/3955/demo/css/datepicker.css
