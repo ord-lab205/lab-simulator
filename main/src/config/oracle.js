d2e660bd953574074616145195cbc6278c2a97ab
bya2 <byaa1972@gmail.com>
2021-10-26 03:06:00 +0900

    [Remove] Remove previous main files

--- main/src/config/oracle.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  user          : process.env.NODE_ORACLEDB_USER,
-  password      : process.env.NODE_ORACLEDB_PASSWORD,
-  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING,
-};
