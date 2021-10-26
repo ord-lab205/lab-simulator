ccc71fa1b5b1571e1135cf1a89494f2c447782a9
bya2 <byaa1972@gmail.com>
2021-10-26 18:51:08 +0900

    [Temp] Write sql query

---
+++ src/sql/index.sql
@@ -0,0 +1,11 @@
+DROP TABLE t_logging__intergrated_node PURGE;
+
+CREATE TABLE t_logging__intergrated_node (
+    idx number,
+    n1 number,
+    n2 number,
+    n3 number,
+    v1 number,
+    v2 number,
+    v3 number
+);
