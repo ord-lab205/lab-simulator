f5dc4c7b5bb2c4e841049d4ba8383be262725453
bya2 <byaa1972@gmail.com>
2021-11-10 02:21:11 +0900

    Write the request logger

---
+++ middleware/logger.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const devOpts = {
+  // prettyPrint: true, // testing purpose. depercated.
+  transport: {
+    target: "pino-pretty",
+    options: {
+      translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
+      ignore: "pid,hostname",
+    }
+  },
+};
+
+const isProd = process.env.NODE_ENV === 'production' ? true : false;
+
+let logger = pino(isProd ? {} : devOpts);
+let reqLogger = expressPino({ logger });
+
+// if (isProd) {
+//   logger = pino();
+//   reqLogger = expressPino();
+// } else {
+//   logger = pino(devOpts);
+//   reqLogger = expressPino({});
+// }
+
+module.exports = {
+  reqLogger,
+  logger,
+};
+
+
+
